14903d3b8a5b76a68b0359a188f960aff229ca72
Rassul Magauin <rassulmagauin@gmail.com>
2022-11-29 09:57:59 +0600

    Final

---
+++ api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+urlpatterns  = [
+    # Todos
+    path('todos/completed',views.TodoCompletedList.as_view()),
+    path('todos/<int:pk>', views.TodoRetrieveUpdateDestroy.as_view()),
+    path('todos/<int:pk>/complete', views.TodoComplete.as_view()),
+    path('todos', views.TodoListCreate.as_view()),
+
+    #Auth
+    path('signup', views.signup),
+    path('login', views.login),
+]
